b8ac25c58b5780e420d494dee74ea1ac957c5f32
siam <samisiam851@gmail.com>
2024-02-10 03:37:41 +0600

    socket implementation left

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,25 +1,56 @@
 import axios from 'axios'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { Context } from '../../Configs/ContextProvider'
+import { MongoUser } from '../../types/types'
+import { IoMdArrowBack } from 'react-icons/io'
 
-type Props = {}
-type inputObject = {
+interface Props { }
+interface inputObject {
   message: string,
+}
+interface Message {
+  _id: string,
+  chatId: string,
+  sender: string,
+  content: string,
+  timeStamp: Date,
 }
 const Chat = (props: Props) => {
   const { chatId } = useParams()
   const { user } = useContext(Context)!
-
+  const navigate = useNavigate()
+  const [messages, setMessages] = useState<Message[]>([])
+  const [secondUser, setSecondUser] = useState<MongoUser | null>(null)
   console.log('chatId', chatId);
 
   const { register, handleSubmit, reset } = useForm<inputObject>();
 
 
+
+
   useEffect(() => {
-    axios.get(`http://localhost:3000/messages/${chatId}`, { headers: { Authorization: `Bearer ${localStorage.getItem('chat-app')}` } }).then(res => console.log(res.data)).catch(err => console.log(err))
+    axios.get(`http://localhost:3000/messages/${chatId}`, { headers: { Authorization: `Bearer ${localStorage.getItem('chat-app')}` } }).then(res => setMessages(res.data)).catch(err => console.log(err))
   }, [])
+
+
+
+  useEffect(() => {
+    const secondUsersMessage = messages?.find(chatUser => chatUser?.sender != user?.email)
+    const user2Email = secondUsersMessage?.sender
+
+    axios.get(`http://localhost:3000/get-single-user?email=${user2Email}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('chat-app')}`
+      }
+    })
+      .then(res => setSecondUser(res.data.user))
+      .catch(err => console.log(err)
+      )
+
+  }, [messages])
+
 
   const onSubmit = async (data: inputObject) => {
     try {
@@ -44,29 +75,67 @@ const Chat = (props: Props) => {
 
 
   return (
-    <div className="flex flex-col h-screen">
-    <div className="flex-1 overflow-auto">
-      {/* Chat messages container */}
-      Chat
-    </div>
-  
-    <div className="mt-auto">
-      {/* Send message form */}
-      <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto flex gap-2 items-center justify-center">
-        {/* Input field with register hook */}
-        <input
-          type="text"
-          {...register('message')} // Register input with name 'message'
-          placeholder="Enter your message"
-          className="border rounded-md p-2 w-full"
-        />
-        {/* Submit button */}
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 mt-2 rounded-md hover:bg-blue-600 transition-colors duration-300">
-          Send
-        </button>
-      </form>
-    </div>
-  </div>
+
+    <>
+
+
+
+      <div className="flex flex-col h-screen pt-2 pb-5 flex-end">
+        <div className='border-b-2 pb-1 mb-2'>
+
+          <div className="flex flex-row w-full gap-3 ps-2 justify-start items-center text-gray-700">
+          <button className="text-white " title='back' onClick={() => navigate(-1)}>
+              <IoMdArrowBack className="w-7 h-7 text-blue-500" />
+            </button>
+            <div className=''>
+
+
+              <div
+                style={{ backgroundImage: `url(${secondUser?.photoURL})` }}
+                className="md:w-14 md:h-14 bg-cover bg-center rounded-full "
+              >
+                {/* <img src={user?.photoURL!} className="w-full h-full" alt="" /> */}
+              </div>
+            </div>
+            <div className=''>
+              <h3 className='text-xl font-medium'>{secondUser?.name}</h3>
+              <h4 className='text-sm text-gray-500'>{secondUser?.email}</h4>
+            </div>
+           
+          </div>
+        </div>
+        <div className='overflow-y-scroll h-full  md:px-10 px-5 flex   '>
+          <div id="chat-container" className=" w-full flex flex-col pb-2">
+            <div className="flex-grow"></div> {/* This div will push messages to the bottom */}
+            {messages.map((message) => (
+              <div key={message._id} className={`w-full mb-2 flex ${message.sender === user?.email ? 'justify-end' : 'justify-start'}`}>
+                <div title={new Date(message.timeStamp).toLocaleString()} className={`max-w-xs rounded-lg px-4 py-2 ${message.sender === user?.email ? 'bg-purple-600 text-white self-end' : 'bg-gray-300 text-black self-start'}`}>
+                  {message.content}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+        <div className="border-t-2 mt-2 pt-2">
+          {/* Send message form */}
+          <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto flex gap-2 items-center justify-center">
+            {/* Input field with register hook */}
+            <input
+              type="text"
+              {...register('message')} // Register input with name 'message'
+              placeholder="Enter your message"
+              className="border rounded-md p-2 w-full"
+            />
+            {/* Submit button */}
+            <button type="submit" className="bg-blue-500 text-white px-4 py-2  rounded-md hover:bg-blue-600 transition-colors duration-300">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
